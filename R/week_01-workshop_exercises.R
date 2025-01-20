ce79bebf34763fd7e32a0c68bbedb18d5d08ec77
Jason Grafmiller (Linguistics and Communication) <j.grafmiller@bham.ac.uk>
2025-01-20 09:19:10 +0000

    init upload

---
+++ R/week_01-workshop_exercises.R
@@ -0,0 +1,124 @@
+# ----------------------------------------------------------------------------
+# file: week_01-workshop_exercises.R
+# author: Jason Grafmiller
+# date: 2025-01-20
+# description:
+# Exercises for Week 1 workshop. Answers will be posted after class
+# ----------------------------------------------------------------------------
+
+
+# Exercise 1: Simple Calculations -----------------------------------------
+
+# Instructions: Use R as a calculator to solve these problems.
+# Just enter your code below and type Ctrl/Cmd+Enter
+
+# a) What is 25 + 17?
+
+
+# b) Subtract 56 from 89
+
+
+# c) Multiply 8 by 12
+
+
+# d) Divide 100 by 4
+
+
+# e) Multiply pi by 2
+
+
+
+# Exercise 2: Assigning variables -----------------------------------------
+
+# Instructions: Create variables to store data and use them in calculations.
+
+
+# a) Assign the value 42 to a variable called `x`.
+
+
+# b) Create another variable `y` and assign it the value 18.
+
+
+# c) Add `x` and `y` together and store the result in a variable `z`.
+
+
+# Print the value of `z` to check your result.
+
+
+# d) Create a variable `my_name` and assign it the value of your name as 
+# a string (use quotes " " or ' ')
+
+
+
+# Exercise 3: Basic Functions ---------------------------------------------
+
+# Instructions: Use built-in R functions to perform tasks.
+
+
+# a) Find the square root of 144 with the `sqrt()` function
+
+
+# b) Calculate the sum of 3, 5, and 9 using the `sum()` function.
+
+
+# c) Find the minimum value in the numbers 7, 15, 3, 9 with `min()`.
+
+
+
+
+# Exercise 4: Working with Vectors ----------------------------------------
+
+# Instructions: Create and manipulate vectors.
+
+# a) Create a vector named `numbers` containing the values 2, 4, 6, 8, 10.
+
+
+# b) Find the mean (average) of the values in `numbers`.
+
+
+# c) Add 5 to each element of the `numbers` vector.
+
+
+# d) Find the length of `numbers` with `length()`
+
+
+# Exercise 5: Logical Comparisons -----------------------------------------
+
+# Instructions: Perform logical comparisons and understand TRUE/FALSE outputs.
+
+# a) Check if 15 is greater than 10
+
+
+# b) Check if 7 is equal to 9
+
+
+# c) Check if `x` is not equal to `y`
+
+
+# d) Check if the length of `numbers` is greater than or equal to 5
+
+
+# Exercise 6: Data Frames -------------------------------------------------
+
+# Instructions: Create and manipulate a data frame.
+
+# a) Create a data frame named `students` with the following data:
+# Name: "Buster", "Charlie", "Harold"
+# Age: 22, 25, 23
+# Score: 88, 76, 90
+
+
+
+# b) Display the structure of the `students` data frame using the `str()` function.
+
+
+# c) Show the `score` column from the data frame
+
+
+# c) Calculate the average score of all students using `mean()`
+
+
+# d) Round the output of `mean()` to 2 decimal places with `round(..., digits = 2)`
+
+
+
