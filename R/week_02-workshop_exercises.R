172ad49d4af2422852ff06e9b8156f56fd9861fc
Jason Grafmiller (Linguistics and Communication) <j.grafmiller@bham.ac.uk>
2025-01-26 11:51:30 +0000

    init upload

---
+++ R/week_02-workshop_exercises.R
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------
+# file: week_02-workshop_exercises.R
+# author: Jason Grafmiller
+# date: 2025-01-26
+# description:
+# Exercises for Week 2 workshop. Full code will be posted after class
+# ----------------------------------------------------------------------------
+
+# 
