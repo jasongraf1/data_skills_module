c64c033186ecfec767bc1e6e4dda23db6c0991f6
Jason Grafmiller (Linguistics and Communication) <j.grafmiller@bham.ac.uk>
2025-02-10 10:28:17 +0000

    init upload

---
+++ R/week_04-Knafflic_tickets.R
@@ -0,0 +1,39 @@
+# -------------------------------------------------------------
+# file: week_04-Knafflic_tickets.R
+# author: Jason Grafmiller
+# date: 2025-02-10
+# description:
+# 
+# -------------------------------------------------------------
+
+# Setup 
+
+
+
+
+# Load the dataset --------------------------------------------
+
+
+
+
+# Horrible bar plot -------------------------------------------
+
+# Try to replicate the bad bar plot from Knafflic.
+# Hints:
+# - include `fill = type` inside `aes()`
+# - use `position = "dodge"` in `geom_col()` to set the 
+# columns side-by-side 
+
+
+# Better line plot --------------------------------------------
+
+
+
+
+# Save your plot ----------------------------------------------
+
+# Use the "Export" menu in the "plots" tab
+
+
+
+# Now try with `ggsave()`  
